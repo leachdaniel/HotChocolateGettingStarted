c8e3a3f03d44553dff508c369d4af08990d25ee0
Daniel Leach <dleach@webstaurantstore.com>
2024-10-01 14:42:47 -0400

    DataLoader vs manual selection testing and loading in the ancestor resolver

---
+++ Extensions.cs
@@ -0,0 +1,24 @@
+﻿namespace HotChocolateGettingStarted
+{
+    public static class DataLoaderExtensions
+    {
+        public static async Task<IReadOnlyDictionary<TKeySource, TValue>> LoadAsDictionary<TKey, TValue, TKeySource>(
+            this IDataLoader<TKey, TValue> @this,
+            IEnumerable<TKeySource> keys,
+            CancellationToken cancellationToken = default) where TKeySource : TKey where TKey : notnull
+        {
+            var keysList = keys.Cast<TKey>().AsReadOnlyList();
+
+            var results = await @this.LoadAsync(keysList, cancellationToken);
+
+            var kvps = results
+                .Where((_, i) => i < keysList.Count)
+                .Select((x, i) => KeyValuePair.Create((TKeySource) keysList[i], x));
+
+            return new Dictionary<TKeySource, TValue>(kvps);
+        }
+
+        public static IReadOnlyList<T> AsReadOnlyList<T>(this IEnumerable<T> @this)
+            => @this as IReadOnlyList<T> ?? @this?.ToArray() ?? Array.Empty<T>();
+    }
+}
--- Query.cs
+++ Query.cs
@@ -1,4 +1,8 @@
-﻿namespace HotChocolateGettingStarted.Types;
+﻿using System.Runtime.InteropServices;
+using HotChocolate.Execution.Processing;
+using HotChocolate.Resolvers;
+
+namespace HotChocolateGettingStarted.Types;
 
 [QueryType]
 public static class Query
@@ -11,8 +15,16 @@ public static class Query
     }
 
     /*
-    performance problems: 
-    takes about 160ms
+    performance problems when using DataLoader relationships
+    takes about 160ms in debug
+    takes 15-20ms regular
+
+    using ResolverContext to set a state
+    debug mode takes 14-20ms
+
+    regular mode takes 4-10ms
+
+
 
     query {
 
@@ -35,11 +47,91 @@ public static class Query
 }
      */
 
-    public static async Task<IEnumerable<Book>> GetBooks(IReadOnlyCollection<int> bookIds, [Service] BookDataLoader bookDataLoader, CancellationToken cancellationToken)
+    //[IsSelected("reviews")]
+    public static async Task<IEnumerable<Book>> GetBooks(IReadOnlyCollection<int> bookIds, IResolverContext context, [Service] BookDataLoader bookDataLoader, CancellationToken cancellationToken)
     {
+
+        var selections = context.GetSelections((ObjectType)context.Selection.Type.NamedType());
+
+        var loadAndSetBookReviewsTask = LoadAndSetBookReviews(bookIds, context, cancellationToken);
+
         var books = await bookDataLoader.LoadAsync(bookIds, cancellationToken);
 
+        var loadAuthorsAndReviewsTask = LoadAndSetAuthorsAndAuthorReviews(books, context, selections, cancellationToken);
+
+        await Task.WhenAll(loadAuthorsAndReviewsTask, loadAndSetBookReviewsTask);
+
         return books;
+    }
+
+    private static async Task LoadAndSetAuthorsAndAuthorReviews(IReadOnlyCollection<Book> books, IResolverContext context, IReadOnlyList<ISelection> selections, CancellationToken cancellationToken)
+    {
+        var authorField = selections.FirstOrDefault(s => s.Field.Name.Equals("author"));
+        var isAuthorSelected = authorField is not null;
+
+        var authorsTask = Task.FromResult(
+            (IReadOnlyDictionary<int, Author>) Array.Empty<Author>()
+                .ToDictionary(_ => 0));
+
+        var authorReviewsTask = Task.CompletedTask;
+
+        if (isAuthorSelected)
+        {
+            var authorIds = books.Select(x => x.AuthorId)
+                .ToHashSet();
+
+            authorsTask = context.Service<AuthorDataLoader>()
+                .LoadAsDictionary(authorIds, cancellationToken);
+
+            var authorSelections = context.GetSelections((ObjectType) authorField!.Type.NamedType(), authorField);
+
+            var isAuthorReviewsSelected = authorSelections.Any(x => x.Field.Name.Equals("reviews"));
+
+            if (isAuthorReviewsSelected)
+            {
+                authorReviewsTask = LoadAndSetAuthorReviews(authorsTask, context, cancellationToken);
+            }
+
+            context.SetScopedState("AuthorsByAuthorId", await authorsTask);
+        }
+
+        await Task.WhenAll(authorReviewsTask, authorsTask);
+    }
+
+    private static async Task LoadAndSetAuthorReviews(Task<IReadOnlyDictionary<int, Author>> authorsTask, IResolverContext context, CancellationToken cancellationToken)
+    {
+        var authors = await authorsTask;
+
+        var authorIds = authors.Keys.AsReadOnlyList();
+
+        var authorReviews = await context.Service<AuthorReviewByAuthorIdDataLoader>().LoadAsync(authorIds, cancellationToken);
+
+        var reviewIds = authorReviews.SelectMany(x => x.Select(r => r.ReviewId)).ToHashSet();
+
+        var reviews = await context.Service<ReviewDataLoader>().LoadAsDictionary(reviewIds, cancellationToken);
+
+        var reviewsByAuthorIdLookup = authorReviews
+            .SelectMany(x => x.Select(rl => new { rl, r = reviews.GetValueOrDefault(rl.ReviewId) }))
+            .Where(x => x.r is not null)
+            .ToLookup(x => x.rl.AuthorId, x => x.r);
+
+        context.SetScopedState("ReviewsByAuthorId", reviewsByAuthorIdLookup);
+    }
+
+    private static async Task LoadAndSetBookReviews(IReadOnlyCollection<int> bookIds, IResolverContext context, CancellationToken cancellationToken)
+    {
+        var bookReviews = await context.Service<BookReviewByBookIdDataLoader>().LoadAsync(bookIds, cancellationToken);
+
+        var reviewIds = bookReviews.SelectMany(x => x.Select(r => r.ReviewId)).ToHashSet();
+
+        var reviews = await context.Service<ReviewDataLoader>().LoadAsDictionary(reviewIds, cancellationToken);
+
+        var reviewsByAuthorIdLookup = bookReviews
+            .SelectMany(x => x.Select(rl => new { rl, r = reviews.GetValueOrDefault(rl.ReviewId) }))
+            .Where(x => x.r is not null)
+            .ToLookup(x => x.rl.BookId, ar => ar.r);
+
+        context.SetScopedState("ReviewsByBookId", reviewsByAuthorIdLookup);
     }
 }
 
@@ -48,7 +140,17 @@ public static class Query
 public static class BookRelationships
 {
 
-    public static async Task<IEnumerable<Review>> GetReviews(
+    public static IEnumerable<Review> GetReviews(
+        [Parent] Book book,
+        IResolverContext context,
+        CancellationToken cancellationToken)
+    {
+        var reviewLookup = context.GetScopedState<ILookup<int, Review>>("ReviewsByBookId");
+
+        return reviewLookup[book.BookId];
+    }
+
+    private static async Task<IEnumerable<Review>> GetReviewsWithDataLoader(
         [Parent] Book book, 
         [Service] BookReviewByBookIdDataLoader bookReviewByBookIdDataLoader,
         [Service] ReviewDataLoader reviewDataLoader,
@@ -63,7 +165,17 @@ public static class BookRelationships
         return reviews;
     }
 
-    public static async Task<Author> GetAuthor(
+    public static Author GetAuthor(
+        [Parent] Book book,
+        IResolverContext context,
+        CancellationToken cancellationToken)
+    {
+        var authors = context.GetScopedState<IReadOnlyDictionary<int, Author>>("AuthorsByAuthorId");
+
+        return authors.GetValueOrDefault(book.AuthorId);
+    }
+
+    private static async Task<Author> GetAuthorWithDataLoader(
         [Parent] Book book,
         [Service] AuthorDataLoader authorDataLoader,
         CancellationToken cancellationToken)
@@ -78,7 +190,7 @@ public static class BookRelationships
 public static class AuthorRelationships
 {
 
-    public static async Task<IEnumerable<Review>> GetReviews(
+    private static async Task<IEnumerable<Review>> GetReviewsWithDataLoaders(
         [Parent] Author author,
         [Service] AuthorReviewByAuthorIdDataLoader authorReviewByAuthorIdDataLoader,
         [Service] ReviewDataLoader reviewDataLoader,
@@ -91,6 +203,16 @@ public static class AuthorRelationships
         var reviews = await reviewDataLoader.LoadAsync(reviewIds, cancellationToken);
 
         return reviews;
+    }
+
+    public static IEnumerable<Review> GetReviews(
+        [Parent] Author author,
+        IResolverContext context,
+        CancellationToken cancellationToken)
+    {
+        var reviewLookup = context.GetScopedState<ILookup<int, Review>>("ReviewsByAuthorId");
+
+        return reviewLookup[author.AuthorId];
     }
 }
 
